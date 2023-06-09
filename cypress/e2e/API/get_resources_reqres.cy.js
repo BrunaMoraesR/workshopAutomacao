489d431afeb811a79a75d6e3c9808cf0ab0422d2
Bruna Moraes <brunam.rocha26@gmail.com>
2023-06-09 19:56:49 -0300

    Validação da paginação de recursos

--- cypress/e2e/API/get_resources_reqres.cy.js
+++ cypress/e2e/API/get_resources_reqres.cy.js
@@ -1,4 +1,5 @@
 const baseUrl = 'https://reqres.in/api/unknown';
+
 describe('List resources | Page 1', () => {
   it('Validação de status code 200', () => {
     cy.request({
@@ -16,6 +17,29 @@ describe('List resources | Page 1', () => {
       const body = res.body;
       cy.log(body)
       expect(body.page).to.eq(1)
+      expect(body.data.length).to.eq(6)
+    })
+  })
+})
+
+describe('List resources | Page 2', () => {
+  const url = baseUrl + '?page=2'
+  it('Validação de status code 200', () => {
+    cy.request({
+      method: 'GET',
+      url: url
+    }).then((res) => {
+      expect(res.status).to.eq(200)
+    })
+  })
+  it('Validação do body', () => {
+    cy.request({
+      method: 'GET',
+      url: url
+    }).then((res) => {
+      const body = res.body;
+      cy.log(body)
+      expect(body.page).to.eq(2)
       expect(body.data.length).to.eq(6)
     })
   })
