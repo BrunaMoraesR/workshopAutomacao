5b857ef25e1c5a56fad982e8ea285e29651d9a89
Bruna Moraes <brunam.rocha26@gmail.com>
2023-06-09 19:57:48 -0300

    Validação de página de recursos sem dados

--- cypress/e2e/API/get_resources_reqres.cy.js
+++ cypress/e2e/API/get_resources_reqres.cy.js
@@ -44,3 +44,26 @@ describe('List resources | Page 2', () => {
     })
   })
 })
+
+describe('List resources | Page 3 | No results', () => {
+  const url = baseUrl + '?page=3'
+  it('Validação de status code 200', () => {
+    cy.request({
+      method: 'GET',
+      url: url
+    }).then((res) => {
+      expect(res.status).to.eq(200)
+    })
+  })
+  it('Validação do body', () => {
+    cy.request({
+      method: 'GET',
+      url: url
+    }).then((res) => {
+      const body = res.body;
+      cy.log(body)
+      expect(body.page).to.eq(3)
+      expect(body.data.length).to.eq(0)
+    })
+  })
+})
