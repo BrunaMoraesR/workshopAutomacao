b70351a09f4faa3528a172bbacf5d1d30302d31a
Bruna Moraes <brunam.rocha26@gmail.com>
2023-06-09 20:23:23 -0300

    Validação de criação de usuário inválido

--- cypress/e2e/API/post_users_reqres.cy.js
+++ cypress/e2e/API/post_users_reqres.cy.js
@@ -28,3 +28,70 @@ describe('Create user | Name and job', () => {
     })
   })
 })
+
+describe('Create user | Name', () => {
+  const requestBody = {
+    "name": "morpheus"
+  }
+  it('Validação de status code 201', () => {
+    cy.request({
+      method: 'POST',
+      url: baseUrl,
+      body: requestBody
+    }).then((res) => {
+      expect(res.status).to.eq(201)
+    })
+  })
+  it('Validação do body', () => {
+    cy.request({
+      method: 'POST',
+      url: baseUrl,
+      body: requestBody
+    }).then((res) => {
+      const body = res.body;
+      cy.log(body)
+      expect(body).to.have.keys('name', 'job', 'id', 'createdAt')
+      expect(body.name).to.eq(requestBody.name)
+      expect(body.job).to.be.null
+    })
+  })
+})
+
+describe('Create user | Job', () => {
+  const requestBody = {
+    "job": "leader"
+  }
+  it('Validação de status code 201', () => {
+    cy.request({
+      method: 'POST',
+      url: baseUrl,
+      body: requestBody
+    }).then((res) => {
+      expect(res.status).to.eq(201)
+    })
+  })
+  it('Validação do body', () => {
+    cy.request({
+      method: 'POST',
+      url: baseUrl,
+      body: requestBody
+    }).then((res) => {
+      const body = res.body;
+      cy.log(body)
+      expect(body).to.have.keys('name', 'job', 'id', 'createdAt')
+      expect(body.name).to.be.null
+      expect(body.job).to.eq(requestBody.job)
+    })
+  })
+})
+
+describe('Create user | Without body', () => {
+  it('Validação de status code 400', () => {
+    cy.request({
+      method: 'POST',
+      url: baseUrl
+    }).then((res) => {
+      expect(res.status).to.eq(400)
+    })
+  })
+})
