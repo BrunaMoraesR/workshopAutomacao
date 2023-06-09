58e67b118aaaed4840837d9ecd6cf5d1868defac
Bruna Moraes <brunam.rocha26@gmail.com>
2023-06-09 19:34:32 -0300

    Validação de single user

--- cypress/e2e/API/get_reqres.cy.js
+++ cypress/e2e/API/get_reqres.cy.js
@@ -1,8 +1,11 @@
+const baseUrl = 'https://reqres.in';
+
 describe('List Users', () => {
-  it('Deveria retornar status code 200', () => {
+  const url = baseUrl + '/api/users';
+  it('Validação status code 200', () => {
     cy.request({
       method: 'GET',
-      url: 'https://reqres.in/api/users'
+      url: url
     }).then((res) => {
       expect(res.status).to.eq(200)
     })
@@ -10,12 +13,35 @@ describe('List Users', () => {
   it('Validação do body', () => {
     cy.request({
       method: 'GET',
-      url: 'https://reqres.in/api/users'
+      url: url
     }).then((res) => {
       const body = res.body;
       cy.log(res.body)
       expect(body.page).to.eq(1)
       expect(body.data.length).to.eq(6)
+    })
+  })
+})
+
+describe('Single User', () => {
+  const url = baseUrl + '/api/users/1';
+  it('Validação status code 200', () => {
+    cy.request({
+      method: 'GET',
+      url: url
+    }).then((res) => {
+      expect(res.status).to.eq(200)
+    })
+  })
+  it('Validação do body', () => {
+    cy.request({
+      method: 'GET',
+      url: url
+    }).then((res) => {
+      const body = res.body;
+      cy.log(res.body)
+      expect(body.data).to.have.keys('id', 'email', 'first_name', 'last_name', 'avatar')
+      expect(body.data.id).to.eq(1)
     })
   })
 })
